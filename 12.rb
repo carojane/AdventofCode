847f08330411318a71f1baa70d0fbe9b6f7b1426
carojane <carojane@gmail.com>
2016-02-26 22:43:14 -0500

    day 12

---
+++ 12.rb
@@ -0,0 +1,38 @@
+require 'json'
+require 'pry'
+
+def total(data)
+  numbers = data.scan(/-?\d+/)
+  total = 0
+  numbers.each {|x| total += x.to_i }
+  total.to_s
+end
+
+def eval_hash(hash)
+  total = 0
+  if !hash.has_value?("red")
+    hash.each do |k,v|
+      total += eval_hash(v) if v.is_a?(Hash)
+      total += eval_array(v) if v.is_a?(Array)
+      total += v if v.is_a?(Integer)
+    end
+  end
+  total
+end
+
+def eval_array(array)
+  total = 0
+  array.each do |v|
+    total += eval_hash(v) if v.is_a?(Hash)
+    total += eval_array(v) if v.is_a?(Array)
+    total += v if v.is_a?(Integer)
+  end
+  total
+end
+
+data = File.read("./day12input.txt")
+data2 = JSON.parse(data)
+
+
+puts "12.a: " + total(data)
+puts "12.b: " + eval_hash(data2).to_s
